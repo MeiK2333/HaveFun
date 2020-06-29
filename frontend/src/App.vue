38f32d51aaa8065c341bfdd1b90cbd3e8bcd9164
MeiK <meik2333@gmail.com>
2020-06-29 16:40:03 +0800

    Update

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <h1>Hello World!</h1>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  })
+});
+</script>
